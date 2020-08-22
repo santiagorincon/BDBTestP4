1deb9c93a516e46a27fbf29a7914c0e4eeadad95
Santiago Rincón <daniel.rincon@ingenian.com>
2020-08-22 18:08:25 -0500

    first commit

---
+++ src/main/java/com/ejemplosapi/modelo/Producto.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ejemplosapi.modelo;
+
+/**
+ *
+ * @author FABRICA.SOFTWARE6
+ */
+public class Producto {
+    public int id;
+    public String nombre;
+    public float precio;
+
+    public Producto() {
+    }
+
+    public Producto(int id, String nombre, float precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Producto other = (Producto) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
